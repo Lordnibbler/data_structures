76909c86b44825483f1298c8f514342a2895f7fe
Ben Radler <ben@benradler.com>
2014-01-25 13:47:29 -0800

    Basic Node and List with :add method

---
+++ linked_list.rb
@@ -0,0 +1,48 @@
+class Node
+  attr_accessor :value, :next
+
+  def initialize(value=nil)
+    if !value.nil?
+      @value = value
+    else
+      raise "You must provide a value for the node."
+    end
+  end
+end
+
+class SinglyLinkedList
+  attr_accessor :head, :tail
+
+  def initialize(first=nil)
+    if first && first.class == Node
+      @head = first
+    else
+      raise "Your must provide a first node for the list"
+    end
+  end
+
+  def add(value)
+    if @head.nil?
+      # first node, so set it to @head
+      @head = Node.new(value)
+    else
+      # save pointer to @head
+      current_node = @head
+
+      # loop over current_node.next until reaching end of list
+      # end of list means current_node has no next
+      current_node = current_node.next while current_node.next
+
+      # instantiate a new Node, and set it to the last node's next
+      current_node.next = Node.new(value)
+    end
+  end
+
+  def find(value)
+
+  end
+
+  def remove(value)
+
+  end
+end
