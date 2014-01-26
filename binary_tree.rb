967698325db872c10d9c8c20f5ead1238eeb774b
Ben Radler <ben@benradler.com>
2014-01-26 14:46:11 -0800

    Setup for Binary tree implementation

---
+++ binary_tree.rb
@@ -0,0 +1,43 @@
+module BinaryTree
+  class Node
+    attr_accessor :value, :count, :left, :right
+
+    include Enumerable
+
+    # create a root node
+    def initialize(value)
+
+    end
+
+    # counts number of nodes with unique values in tree
+    def size
+
+    end
+
+    # insert a node into the tree
+    def insert(node)
+
+    end
+
+    def each
+
+    end
+
+    # returns all values
+    def values
+
+    end
+
+    # counts total number of nodes in tree
+    def count_all
+
+    end
+
+  protected
+
+    # insert a node into a specific destination in the tree
+    def insert_into(destination, node)
+
+    end
+  end
+end
