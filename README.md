6ffb46b2ed194a598140fa5181e93fbe600d2a67
Ben Radler <ben@benradler.com>
2014-01-26 14:35:18 -0800

    Basic Tree

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Linked List
 In terminal, execute: `./examples/linked_list`
+
+## Tree
+In terminal, execute: `./examples/tree`
---
+++ tree.rb
@@ -0,0 +1,8 @@
+class Node
+  attr_accessor :children, :value
+
+  def initialize(value)
+    @value    = value
+    @children = []
+  end
+end
